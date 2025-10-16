d1402cac3986f65989e25dd19d8f9d318312ad72
LAUTARO ZUCA CORREA <56443876@ceibal.edu.uy>
2025-10-16 02:34:36 -0300

    Primeros pasos en la página estadisticas del jugador

--- frontend/src/pages/estadisticas.jsx
+++ frontend/src/pages/estadisticas.jsx
@@ -2,7 +2,34 @@ import './pages.css';
 
 function Estadisticas() {
   return (
-    <h1>ESTADISTICAS</h1>
+
+    <main className='contenedor'>
+
+      <section className='estadisticaTitulo'>
+        <h1>Mis Estadisticas de Juego</h1>
+      </section>
+
+      <section className='estadisticaTarjertas'>
+
+        <div className='tarjetaEstadistica'>
+          <p>total de juegos</p>
+          <h2>125</h2>
+        </div>
+
+        <div className='tarjetaEstadistica'>
+          <p>juegos completados</p>
+          <h2>78</h2>
+        </div>
+        
+        <div className='tarjetaEstadistica'>
+          <p>horas jugadas</p>
+          <h2>450h</h2>
+        </div>
+
+      </section>
+      
+    </main>
+    
   );
 }
 
